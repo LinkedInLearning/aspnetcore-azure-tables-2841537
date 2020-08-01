41427af1b9c1325af013f5cab2d7385e2ad4f2cc
Ervis Trupja <ervistrupja@outlook.com>
2020-08-02 01:20:29 +0200

    Added Custom Entity and Installed Package

---
+++ EmployeeManagement/Data/Entities/EmployeeEntity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeManagement.Data.Entities
+{
+    public class EmployeeEntity:TableEntity
+    {
+        public EmployeeEntity()
+        {
+        }
+
+        public EmployeeEntity(string department)
+        {
+            PartitionKey = department;
+        }
+
+        public string FullName { get; set; }
+        public string Email { get; set; }
+    }
+}
